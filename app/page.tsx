2fa695f1e0cdb8f5b149e66f5e1cc147a1bd0387
alex19849-dot <alex19849@gmail.com>
2026-05-27 13:08:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -206,7 +206,7 @@ const [touchEndX, setTouchEndX] = useState<number | null>(null);
   const activeChapter = chapters[activeChapterIndex] || "";
 function splitIntoPages(text: string) {
   const words = text.split(" ");
-  const wordsPerPage = 180;
+  const wordsPerPage = 260;
 
   const pages = [];
   for (let i = 0; i < words.length; i += wordsPerPage) {
